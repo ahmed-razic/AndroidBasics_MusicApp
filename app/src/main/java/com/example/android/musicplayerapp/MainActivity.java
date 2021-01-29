85dd5560087047f13bec55448e37f2296761e035
ahmed-razic <ahmed.razic@gmail.com>
2021-01-29 17:24:26 +0100

    added OnCompletionListener

--- app/src/main/java/com/example/android/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayerapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.billie_jean);
+        mediaPlayer = MediaPlayer.create(this, R.raw.planet_sarkar);
 
         Button playButton = findViewById(R.id.play);
         Button pauseButton = findViewById(R.id.pause);
@@ -25,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
         EditText inputTimeEditView = findViewById(R.id.input_time);
 
 
-        playButton.setOnClickListener(v -> mediaPlayer.start());
-
+        playButton.setOnClickListener(v -> {
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(mp -> Toast.makeText(MainActivity.this, "I'm done", Toast.LENGTH_LONG).show());
+        });
         pauseButton.setOnClickListener(v -> mediaPlayer.pause());
 
         seekButton.setOnClickListener(v -> {
@@ -34,5 +37,7 @@ public class MainActivity extends AppCompatActivity {
             mediaPlayer.seekTo(inputTime);
             mediaPlayer.start();
         });
+
+
     }
 }
